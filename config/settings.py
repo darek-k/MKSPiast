510525d94d6f49f4e0cdc92111bb698782e9bc93
kuzma <kuzma.dariusz@gmail.com>
2021-04-14 11:13:01 +0200

    Debug = False

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG_VALUE = (os.environ.get('DEBUG_VALUE') == 'True')
 DEBUG_PROPAGATE_EXCEPTIONS = True
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mkspiast.herokuapp.com']
 
