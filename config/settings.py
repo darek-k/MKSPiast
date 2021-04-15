53b390aa9e827ea0353d781169dd740f1286888d
kuzma <kuzma.dariusz@gmail.com>
2021-04-15 10:31:20 +0200

    fix cloudinary bug

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG_PROPAGATE_EXCEPTIONS = True
-DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mkspiast.herokuapp.com']
 
